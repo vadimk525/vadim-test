10812406917c6520f8ae1096bd6f6279ba0b6980
vadimk525 <v.karpukhin@danaflex.ru>
2024-01-16 10:29:21 +0300

    Создал в web.php группу для маршрутов административной части

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function(){
+
+  });
 
 Route::get('/', function () {
     return view('welcome');
