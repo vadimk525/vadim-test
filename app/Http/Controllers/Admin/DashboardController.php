dd627a21425336e0c7079e7131878da97b691443
vadimk525 <v.karpukhin@danaflex.ru>
2024-01-29 16:12:18 +0300

    Создал метод для отрисовки дашборда
    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    //Dashboard
+    public function dashboard() {
+        return view('admin.dashboard');
+    }
 }
