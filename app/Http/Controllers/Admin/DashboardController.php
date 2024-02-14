337f0bb92f70112b718ebc16a159c3a364239f48
vadimk525 <v.karpukhin@danaflex.ru>
2024-02-14 17:13:50 +0300

    Создал метод Dash_Boardadm_ для отрисовки в файле Das_B

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    //Dashboardadm
+    public function dashboardadm() {
+        return view('dashboardadm');
+    }
 }
