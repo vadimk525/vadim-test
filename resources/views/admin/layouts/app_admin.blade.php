7780db94a307a085e92a9e6fca78fc825b9aaf04
vadimk525 <v.karpukhin@danaflex.ru>
2024-02-22 16:27:54 +0300

    Прописал код в app_admin как посоветовал Арсен
    Прописал код в app_admin.blade.php как посоветовал Арсен, а ранее было по аналоги как в файле app.blade.php. Там был другой синтаксис. Использовался Slot

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Hello</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -31,7 +31,7 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @yield('content')
             </main>
         </div>
     </body>
