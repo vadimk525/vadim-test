c85b0095c139ff90a8bd1fa4df593b1883b9d09a
vadimk525 <v.karpukhin@danaflex.ru>
2024-02-29 12:39:02 +0300

    Добавил элементы интерфейса адм странички в файле dashboardadm_
    Добавил элементы интерфейса административной странички в файле dashboardadm,blade,php. К сожалению страничка получилась не как у преподавателя. Возможно надо посмотреть что-то в родительских файлах.

--- resources/views/admin/dashboardadm.blade.php
+++ resources/views/admin/dashboardadm.blade.php
@@ -5,7 +5,49 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ 'Admin Test!' }}
+                    {{-- {{ 'Admin Test!' }}//закоментировал строчку, ниже вместо этой строки новые html_элементы//--}}
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="jumbotron">
+                                    <p><span class="label label-primary">Категорий 0</span></p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="jumbotron">
+                                    <p><span class="label label-primary">Материалов 0</span></p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="jumbotron">
+                                    <p><span class="label label-primary">Посетителей 0</span></p>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="jumbotron">
+                                    <p><span class="label label-primary">Сегодня 0</span></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <a class="btn btn-block btn-default" href="#">Создать категорию</a>
+                                <a class="list-group-item" href="#"></a>
+                                <h4 class="list-group-item-heading">Категория первая</h4>
+                                <p class="list-group-item-text">
+                                    Кол-во материалов
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <a class="btn btn-block btn-default" href="#">Создать материал</a>
+                            <a class="list-group-item" href="#"></a>
+                            <h4 class="list-group-item-heading">Материал первый</h4>
+                            <p class="list-group-item-text">
+                                Категория
+                            </p>
+                    </div>
                 </div>
             </div>
         </div>
