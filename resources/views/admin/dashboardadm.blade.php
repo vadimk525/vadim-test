2835db5d4baae66b0b5a9c952fb34aa44dcc2dbd
vadimk525 <v.karpukhin@danaflex.ru>
2024-04-09 16:54:45 +0300

    Исправил ошибку в HTML разметке

--- resources/views/admin/dashboardadm.blade.php
+++ resources/views/admin/dashboardadm.blade.php
@@ -32,19 +32,21 @@
                         <div class="row">
                             <div class="col-sm-6">
                                 <a class="btn btn-block btn-default" href="#">Создать категорию</a>
-                                <a class="list-group-item" href="#"></a>
-                                <h4 class="list-group-item-heading">Категория первая</h4>
-                                <p class="list-group-item-text">
+                                <a class="list-group-item" href="#">
+                                    <h4 class="list-group-item-heading">Категория первая</h4>
+                                    <p class="list-group-item-text">
                                     Кол-во материалов
-                                </p>
+                                    </p>
+                                </a>
                             </div>
                             <div class="col-sm-6">
                                 <a class="btn btn-block btn-default" href="#">Создать материал</a>
-                                <a class="list-group-item" href="#"></a>
-                                <h4 class="list-group-item-heading">Материал первый</h4>
-                                <p class="list-group-item-text">
+                                <a class="list-group-item" href="#">
+                                    <h4 class="list-group-item-heading">Материал первый</h4>
+                                    <p class="list-group-item-text">
                                     Категория
-                                </p>
+                                    </p>
+                                </a>
                             </div>
                         </div>
 
