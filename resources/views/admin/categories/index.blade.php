b50c78f1105d669e3542b089e0c122ababf0a10e
vadimk525 <v.karpukhin@danaflex.ru>
2024-04-19 15:14:03 +0300

    Доделал по образцу преподавателя страничку "Категории"

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,12 +4,38 @@
 
 <div class="container">
 
-@component('admin.components.breadcrumb')
+    @component('admin.components.breadcrumb')
     @slot('title') Список категорий @endslot
     @slot('parent') Главная @endslot
     @slot('active') Категории @endslot
-@endcomponent
+    @endcomponent
 
+    <hr>
+
+    <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fafa-plus-square-o"></i> Создать категорию</a>
+    <table class="table table-striped">
+        <thread>
+            <th>Наименование</th>
+            <th>Публикация</th>
+            <th class="text-right">Действие</th>
+        </thread>
+        <tbody>
+            @forelse ($categories as $category)
+                <tr>
+                    <td>{{$category->title}}</td>
+                    <td>{{$category->published}}</td>
+                    <td>
+                        <a href="{{route('admin.category.edit', ['id'=>$category->id])}}"><i class="fafa-edit"></i></a>
+                    </td>
+                </tr>
+
+            @empty
+                <tr>
+                    <td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
 
 @endsection
