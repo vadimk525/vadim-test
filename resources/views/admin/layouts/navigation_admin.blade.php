108c1e17e9f4f849b5507f78e6682424f1f197ca
vadimk525 <v.karpukhin@danaflex.ru>
2024-04-23 10:29:14 +0300

    Добавил ссылку на страничку списка категорий index вместо заглушки # в навигационном меню

--- resources/views/admin/layouts/navigation_admin.blade.php
+++ resources/views/admin/layouts/navigation_admin.blade.php
@@ -63,7 +63,7 @@
                 <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                   <div class="py-1" role="none">
                     <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Категории</a>
+                    <a href="{{route('admin.category.index')}}" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Категории</a>
                     <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Материалы</a>
                   </div>
                 </div>
