bb2260bf4fb63e17ff4d05f1d11068e7be39600e
vadimk525 <v.karpukhin@danaflex.ru>
2024-05-27 15:14:55 +0300

    Модифицировал файл модели создав запись один ко многим для создания вложенных категорий
    Модифицировал файл модели создав запись один ко многим для создания вложенных категорий

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    //Get children Category
+    public function children() {
+        return $this -> hasMany(self::class , 'parent_id'); //первый параметр можно записать так - 'app\Catrgory'
+    }
 }
